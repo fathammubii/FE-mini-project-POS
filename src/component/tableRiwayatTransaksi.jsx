f1e9f33a68498277aad16e63b0e7ead91f8fb6e4
Muhammad Fatham M A <fathamubina1@gmail.com>
2024-06-24 09:42:20 +0700

    fetch riwayat transaksi

--- src/component/tableRiwayatTransaksi.jsx
+++ src/component/tableRiwayatTransaksi.jsx
@@ -1,17 +1,30 @@
+import React from 'react';
 
-
-const tableRiwayatTransaksi = () => {
+const tableRiwayatTransaksi = ({transactions}) => {
     return(
-        <table className='table-auto text-left w-full border-2 border-gray-200'>
+        <table className='table-auto text-center w-full'>
         <thead>
             <tr>
-                <th>Tanggal Transaksi</th>
-                <th>ID Transaksi</th>
-                <th>Total Harga</th>
-                <th>Total Bayar</th>
-                <th>Action</th>
+                <th className='border-y-2 border-gray-200 p-2'>Tanggal Transaksi</th>
+                <th className='border-y-2 border-gray-200 p-2'>ID Transaksi</th>
+                <th className='border-y-2 border-gray-200 p-2'>Total Harga</th>
+                <th className='border-y-2 border-gray-200 p-2'>Total Bayar</th>
+                <th className='border-y-2 border-gray-200 p-2'>Action</th>
             </tr>
         </thead>
+        <tbody>
+            {transactions.map((transactions, index) => (
+                <tr key={index}>
+                    <td>{new Date(transactions.transaction_date).toLocaleString()}</td>
+                    <td>{index+1}</td>
+                    <td>{transactions.total_amount}</td>
+                    <td>{transactions.total_pay}</td>
+                    <td><button>Detail Transaksi</button></td>
+                </tr>
+
+            ))}
+
+        </tbody>
 
     </table>
     )
--- src/pages/riwayatTransaksi.jsx
+++ src/pages/riwayatTransaksi.jsx
@@ -1,12 +1,29 @@
 
+import { useEffect, useState } from "react";
 import TableRiwayatTransaksi from "../component/tableRiwayatTransaksi";
+import axios from "axios";
 
 const riwayatTransasksi = () => {
+
+    const [transactions, setTransactions] = useState([]);
+
+    useEffect(() => {
+        const fetchTransactions = async () => {
+
+            try{
+                const response = await axios.get('http://localhost:8080/pos/api/listtransaksi');
+                setTransactions(response.data);
+            } catch(error){
+                console.log('Error fetching transactions: ', error);
+            }
+        };
+        fetchTransactions();
+    },[]);
 
     return (
         <div className="text-left">
             <h1>Riwayat Transaksi</h1>
-            <TableRiwayatTransaksi/>
+            <TableRiwayatTransaksi transactions={transactions}/>
         </div>
 
     )
