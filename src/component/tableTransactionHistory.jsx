58150c51a96fe5275f41cec3f74bf1f08a875445
Muhammad Fatham M A <fathamubina1@gmail.com>
2024-06-25 09:16:59 +0700

    pagination pada halaman transaction history

--- src/component/tableTransactionHistory.jsx
+++ src/component/tableTransactionHistory.jsx
@@ -2,34 +2,65 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
-const tableTransactionHistory = ({transactions}) => {
-    return(
-        <table className='table-auto text-center w-full'>
-        <thead>
-            <tr>
-                <th className='border-y-2 border-gray-200 p-2'>Tanggal Transaksi</th>
-                <th className='border-y-2 border-gray-200 p-2'>ID Transaksi</th>
-                <th className='border-y-2 border-gray-200 p-2'>Total Harga</th>
-                <th className='border-y-2 border-gray-200 p-2'>Total Bayar</th>
-                <th className='border-y-2 border-gray-200 p-2'>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            {transactions.map((transactions, index) => (
-                <tr key={index}>
-                    <td>{new Date(transactions.transaction_date).toLocaleString()}</td>
-                    <td>{transactions.transaction_id}</td>
-                    <td>{transactions.total_amount}</td>
-                    <td>{transactions.total_pay}</td>
-                    <td><button><Link to={`/transaction-detail/${transactions.transaction_id}`}>Detail Transaksi</Link></button></td>
-                </tr>
+const TableTransactionHistory = ({ transactions, transactionsPerPage, totalTransactions, paginate, currentPage }) => {
+    const pageNumbers = [];
 
-            ))}
+    for (let i = 1; i <= Math.ceil(totalTransactions / transactionsPerPage); i++) {
+        pageNumbers.push(i);
+    }
 
-        </tbody>
+    return (
+        <>
+            <table className='table-auto text-center w-full'>
+                <thead>
+                    <tr>
+                        <th className='border-y-2 border-gray-200 p-2'>Tanggal Transaksi</th>
+                        <th className='border-y-2 border-gray-200 p-2'>ID Transaksi</th>
+                        <th className='border-y-2 border-gray-200 p-2'>Total Harga</th>
+                        <th className='border-y-2 border-gray-200 p-2'>Total Bayar</th>
+                        <th className='border-y-2 border-gray-200 p-2'>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {transactions.map((transaction, index) => (
+                        <tr key={index}>
+                            <td>{new Date(transaction.transaction_date).toLocaleString()}</td>
+                            <td>{transaction.transaction_id}</td>
+                            <td>{transaction.total_amount}</td>
+                            <td>{transaction.total_pay}</td>
+                            <td><button><Link to={`/transaction-detail/${transaction.transaction_id}`}>Detail Transaksi</Link></button></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
 
-    </table>
+            <div className='flex justify-center mt-4'>
+                <button 
+                    onClick={() => paginate(currentPage - 1)} 
+                    disabled={currentPage === 1}
+                    className='mx-1 px-3 py-1 border rounded'
+                >
+                    Previous
+                </button>
+                {pageNumbers.map(number => (
+                    <button 
+                        key={number} 
+                        onClick={() => paginate(number)} 
+                        className={`mx-1 px-3 py-1 border rounded ${currentPage === number ? 'bg-gray-300' : ''}`}
+                    >
+                        {number}
+                    </button>
+                ))}
+                <button 
+                    onClick={() => paginate(currentPage + 1)} 
+                    disabled={currentPage === pageNumbers.length}
+                    className='mx-1 px-3 py-1 border rounded'
+                >
+                    Next
+                </button>
+            </div>
+        </>
     )
 }
 
-export default tableTransactionHistory;
+export default TableTransactionHistory;
--- src/pages/transactionHistoryPage.jsx
+++ src/pages/transactionHistoryPage.jsx
@@ -1,31 +1,41 @@
-
 import { useEffect, useState } from "react";
 import TableTransactionHistory from "../component/tableTransactionHistory";
 import axios from "axios";
 
 const TransactionHistoryPage = () => {
-
     const [transactions, setTransactions] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [transactionsPerPage] = useState(10);
 
     useEffect(() => {
         const fetchTransactions = async () => {
-
-            try{
+            try {
                 const response = await axios.get('http://localhost:8080/pos/api/listtransaksi');
                 setTransactions(response.data);
-            } catch(error){
+            } catch (error) {
                 console.log('Error fetching transactions: ', error);
             }
         };
         fetchTransactions();
-    },[]);
+    }, []);
+
+    const indexOfLastTransaction = currentPage * transactionsPerPage;
+    const indexOfFirstTransaction = indexOfLastTransaction - transactionsPerPage;
+    const currentTransactions = transactions.slice(indexOfFirstTransaction, indexOfLastTransaction);
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     return (
         <div className="text-left">
             <h1>Riwayat Transaksi</h1>
-            <TableTransactionHistory transactions={transactions}/>
+            <TableTransactionHistory 
+                transactions={currentTransactions} 
+                transactionsPerPage={transactionsPerPage} 
+                totalTransactions={transactions.length} 
+                paginate={paginate} 
+                currentPage={currentPage}
+            />
         </div>
-
     )
 };
 
